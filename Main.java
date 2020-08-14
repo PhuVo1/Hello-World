3c4cc5d36f00128fc2552e6bd99f196d0e211ca8
PhuVo1 <69691703+PhuVo1@users.noreply.github.com>
2020-08-14 11:01:07 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Phu Vo");
+		System.out.println("My favorite favorite hobbies is playing games");
+		System.out.println("I want to go to UCSF for college");
+		System.out.println("I want to major in nursing");
+		System.out.println("My favorite game right now is For Honor");
+	}
+}
